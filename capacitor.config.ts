1b3e1ebc02b40b71d28dd5b1fa0631646c235225
MarioBasCo <mario-basilio1998@hotmail.com>
2023-02-17 23:31:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'apuestas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
